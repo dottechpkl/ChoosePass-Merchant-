7687c65c02ea5c40ea462bad7030dd709738cf9b
Dottechnologies <pchandra@DOTmac3.local>
2014-05-29 10:37:44 +0530

    Initial Commit

---
+++ ChoosePass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChoosePass
+//
+//  Created by Dottechnologies on 5/29/14.
+//  Copyright (c) 2014 Dottechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
